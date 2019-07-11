439f1f0dea76f4fabe734494f935f08a2c579f0a
khalil saidane <khalilsaidane@khalils-MacBook-Pro.local>
2019-07-11 09:16:04 +0100

    first commit

---
+++ src/main/java/com/khalil/saidane/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.khalil.saidane.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+}
